f247194eab73fde9cc2b5c462271f36a897bc038
Apurba Kumar Ganguly_12 <sahebganguly1234@gmail.com>
2021-05-26 12:05:02 +0530

    p

--- src/App.js
+++ src/App.js
@@ -4,103 +4,67 @@ import React, { useState,Component,useEffect } from 'react';
 import Board from './Board';
 import Card from './Card';
 import {useSelector} from 'react-redux';
+import {DragDropContext,Droppable,Draggable} from 'react-beautiful-dnd';
 
 function App() {
-      const score=useSelector(state=>state.reducer.cond);    
-      const gameover=useSelector(state=>state.reducer.over);
-      const [sviewscore,setsviewscore]=useState(false);
+//       const score=useSelector(state=>state.reducer.cond);    
+//       const gameover=useSelector(state=>state.reducer.over);
+//       const [sviewscore,setsviewscore]=useState(false);
 
-useEffect(()=>{
-  if(gameover){
-    alert("Thanks gor playing :)");
-  }
-},[])
+// useEffect(()=>{
+//   if(gameover){
+//     alert("Thanks gor playing :)");
+//   }
+// },[])
 
-
-const viewscore=()=>{
-if(gameover){
-  alert("Thanks for playing :)");
-  setsviewscore(true)
+const onDragend=result=>{
 
 }
-}
+
+// const viewscore=()=>{
+// if(gameover){
+//   alert("Thanks for playing :)");
+//   setsviewscore(true)
+
+// }
+// }
   return (
-    <div className="App">
-      <div className="flexbox"> 
-      <div className="boards">
-        <div >
-        <h3 className="heading" >Domestic Animals</h3>
-        <Board id="board-1" className="board">
-   
-   </Board>
-        </div>
-     
-    {sviewscore?<h1>Score:{score}/12</h1>:
-    <button className="pc" onClick={viewscore}>View Score</button> }
-    <div >
-          <h3 className="heading" >Wild Animals</h3>
-        <Board id="board-2" className="board">
-   
-   </Board>
-   
-        </div>
-      </div>
-      {sviewscore?<h1>Score:{score}/12</h1>:
-    <button className="mobiles" onClick={viewscore} >View Score</button>
-    }
-    <div className="cards">
-    <Card id="card-1" className="card_1" draggable="true" bid="board-2">
-  
-
-</Card>
-<Card id="card-2" className="card_2" draggable="true" bid="board-1">
-
-      
-      </Card>
-      <Card id="card-3" className="card_3" draggable="true" bid="board-1">
-
-        
-      </Card>
-      <Card id="card-4" className="card_4" draggable="true" bid="board-1">
-
-        
-      </Card>
-      <Card id="card-5" className="card_5" draggable="true" bid="board-1">
-
-        
-      </Card>
-      <Card id="card-6" className="card_6" draggable="true" bid="board-2">
-
-        
-      </Card>
-      <Card id="card-7" className="card_7" draggable="true" bid="board-2">
-
-        
-      </Card>
-      <Card id="card-8" className="card_8" draggable="true" bid="board-1">
-
-        
-      </Card>
-      <Card id="card-9" className="card_9" draggable="true" bid="board-1">
-
-        
-      </Card>
-      <Card id="card-10" className="card_10" draggable="true" bid="board-2">
-
-        
-      </Card>
-      <Card id="card-11" className="card_11" draggable="true" bid="board-2">
-
-        
-      </Card>
-      <Card id="card-12" className="card_12" draggable="true" bid="board-2">
-
-        
-      </Card>
-    </div>
+    <DragDropContext
+      onDragEnd={onDragend}
     
-      </div>
-    </div>
+    >
+       <Droppable
+       droppableId="1"
+       >
+         {(provided) => (
+                   <div
+                   {...provided.droppableProps}
+                   ref={provided.innerRef}
+                  style={{width:'600px',height:'500px',backgroundColor:'green'}}
+                 >
+         <Draggable
+         draggableId="2"
+         index="3"
+         
+         >
+         {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      
+                    >
+                      <div style={{width:'200px',height:'40px',backgroundColor:'blue'}}>
+                        hie
+                      </div>
+                      </div>
+         )}
+         </Draggable>
+         {provided.placeholder}
+         </div>
+         )}
+       </Droppable>
+        </DragDropContext>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,35 +1,49 @@
 import React from 'react'
+import {Draggable} from 'react-beautiful-dnd';
 import './Card.css';
 function Card(props) {
 
-    const dragstart=e=>{
+//     const dragstart=e=>{
         
-        const target=e.target;
-        console.log(target);
+//         const target=e.target;
+//         console.log(target);
 
-        e.dataTransfer.setData(props.bid || target.id,target.id);
+//         e.dataTransfer.setData(props.bid || target.id,target.id);
     
-        console.log("card",e);
+//         console.log("card",e);
     
-        setTimeout(() => {
-        target.style.display="none"
-    }, 0);
-    }
-const dragover=e=>{
-    e.stopPropagation();
-}
+//         setTimeout(() => {
+//         target.style.display="none"
+//     }, 0);
+//     }
+// const dragover=e=>{
+//     e.stopPropagation();
+// }
 
     return (
+        <Draggable
+        draggableId={props.id}
+        index={props.id*10}
+        
+        
+        >
+            {(provided)=>{
+
+           
         <div
-        id={props.id}
-        onDragStart={dragstart}
-        onDragOver={dragover}
-        draggable={props.draggable}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        innerRef={provided.innerRef}
+       // id={props.id}
+       // onDragStart={dragstart}
+       // onDragOver={dragover}
+      //  draggable={props.draggable}
         className={props.className}
         >
             {props.children}
         </div>
-        
+         }}
+        </Draggable>
     )
 }
 
